e88da947083d0db25b46a974d5f38842be32aab6
Christian Kunert <christian.kunert@zalando.de>
2015-07-03 07:52:28 +0200

    Added Support for Service Realm, must provide Service Realm credentials

--- greyhound.sh
+++ greyhound.sh
@@ -8,11 +8,36 @@ team_url="https://teams.auth.zalando.com/teams"
 user_url="https://users.auth.zalando.com/employees"
 token_url="https://token.auth.zalando.com/access_token"
 tokeninfo="https://auth.zalando.com/oauth2/tokeninfo?access_token"
+APPJSON='{"application_username":"XXXXXXXXXXXXXX","application_password":"XXXXXXXXXXXXXXXXX"}'
+CLIENTJSON='{"client_id":"XXXXXXXXXXXXXXX","client_secret":"XXXXXXXXXXXXXXXXXXXXXX"}'
+
+json_decode () {
+    echo -n "$1" | jq -r ."$2"
+}
+
+url_encode () {
+    echo -n "$1" | perl -pe 's/([^a-zA-Z0-9])/"%".sprintf("%x", ord($1))/ge'
+}
+
+get_service_token () {
+
+    APP_PASS=$( json_decode "$APPJSON" "application_password" )
+    APP_NAME=$( json_decode "$APPJSON" "application_username" )
+    CLIENT_ID=$( json_decode "$CLIENTJSON" "client_id" )
+    CLIENT_SECRET=$( json_decode "$CLIENTJSON" "client_secret" )
+    AUTH_HDR=$( echo -n "$CLIENT_ID:$CLIENT_SECRET" | base64 | tr -d ' \n' )
+    USER_ID=$( url_encode "$APP_NAME" )
+    USER_PASS=$( url_encode "$APP_PASS" )
+
+    curl -X POST --header "Authorization: Basic $AUTH_HDR" --data "grant_type=password&username=$USER_ID&password=$USER_PASS&scope=uid" "https://auth.zalando.com/oauth2/access_token?realm=/services"
+}
+
 
 show_help() {
 printf "Usage: ./token.sh [-ha] [-t TEAMNAME] [-u USERNAME] [-ir TOKEN]
     Token Helper
     -a              Get an access_token and add to clipboard (OSX Only)
+    -s              Get an access_token from service realm
     -t  TEAMNAME    Retrieves listing from team api
     -u  USERNAME    Get specific user info
     -i  TOKEN       Poll the Tokeninfo endpoint
@@ -30,7 +55,7 @@ login() {
 }
 
 OPTIND=1
-while getopts "hat:u:i:r:" opt; do
+while getopts "hast:u:i:r:" opt; do
     case "$opt" in
         h)
             show_help
@@ -40,6 +65,10 @@ while getopts "hat:u:i:r:" opt; do
             login
             printf "\nToken added to clipboard\n"
             echo $token | pbcopy
+            exit 1
+            ;;
+        s)
+            get_service_token
             exit 1
             ;;
         t)  teamname=$OPTARG
@@ -64,5 +93,7 @@ while getopts "hat:u:i:r:" opt; do
             ;;
     esac
 done
+
+
 
 # EOF
