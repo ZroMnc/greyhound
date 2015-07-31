c0e204767dde05847ca6465af7c391c09e69f9c2
cgmv <christian.viola@zalando.de>
2015-07-31 17:15:14 +0200

    Solved the Error scenario

--- greyhound.sh
+++ greyhound.sh
@@ -6,7 +6,7 @@ teamname=""
 username=""
 team_url="https://teams.auth.zalando.com/api"
 user_url="https://users.auth.zalando.com/employees"
-token_url="https://token.auth.zalando.com/access_token"
+token_url="https://token.auth.zalando.com/access_token?json=true"
 tokeninfo="https://auth.zalando.com/oauth2/tokeninfo?access_token"
 
 APPJSON='{"application_username":"XXXXXXXXXXXXXX","application_password":"XXXXXXXXXXXXXXXXX"}'
@@ -16,6 +16,9 @@ CLIENTJSON='{"client_id":"XXXXXXXXXXXXXXX","client_secret":"XXXXXXXXXXXXXXXXXXXX
 TOKEN_STILL_VALID_MSG="Your Token is still valid"
 TOKEN_INVALID_MSG="Your Token is invalid - requesting a new"
 INVALID_REQUEST_ERROR='"invalid_request"'
+
+#ERROR CODES
+ERROR_401="401"
 
 json_decode () {
     echo -n "$1" | jq -r ."$2"
@@ -54,11 +57,12 @@ printf "Usage: ./token.sh [-har] [-t TEAMNAME] [-u USERNAME] [-i TOKEN]
 login() {
     DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
     if [ -f $DIR/.token ]; then
-        echo 'Here'
+#        echo 'Here'
         token=$( cat $DIR/.token )
         RESPONSE=$(curl -s "$tokeninfo=$token")
         VAR1=$( echo "$RESPONSE" | jq .'error')
         if [ "$VAR1" == "$INVALID_REQUEST_ERROR" ]; then
+            echo 'Blubb'
             printf "$TOKEN_INVALID_MSG\n"
             getToken
         else
@@ -81,15 +85,25 @@ getToken() {
         read -p "Enter Username : " uname
         read -s -p "Enter Password : " pass
         base=$(echo -n "$uname:$pass" | base64)
+        printf "BASE $base \n"
         token=$(curl -s --header  "Authorization: Basic $base" $token_url)
-        VAR2=$( echo "$token" | jq .'code' )
-        if [ "$VAR2" == "401" ]; then
-            printf "Unauthorized\n"
+       # echo $token
+        # Print either an access token or the error code
+        VAR2=$( echo "$token" | jq -r '[.access_token // .code]' | tr -d '[]' )
+        echo $VAR2
+        printf "$VAR2\n"
+        if [[ "$VAR2" == *$ERROR_401* ]]; then
+            printf "Unauthorized - Username or Password Error\n"
+#            echo $ERROR_401
+#            echo $VAR2
             exit 1
         else
-            echo $token > $DIR/.token
-            echo $token | pbcopy
-            printf "\n[+] Token added to clipboard\n"
+            echo $VAR2 | tr -d '""' > $DIR/.token
+#            echo 'Beer'
+            echo $VAR2 | pbcopy
+            export TOKEN=$( cat .token )
+
+#            printf "\n[+] Token added to clipboard\n"
         fi
     }
 
